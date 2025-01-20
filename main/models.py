52e1cae16f3c9aca19c0f3d6d0d02b4975398f15
CPTCIC5 <aryanjainak@gmail.com>
2025-01-20 21:58:06 +0530

    XYZ

--- main/models.py
+++ main/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
+from organization.models import Organization
+
+WORKPLACE_TYPES= (
+    (1, 'Hybrid'),
+    (2, 'On-Site'),
+    (3, 'Remote')
+)
+
+WORK_TYPES= (
+    (1, 'Full-time'),
+    (2, 'Part-time'),
+    (3, 'Contract'),
+    (4, 'Temperory'),
+    (5, 'Other'),
+    (6, 'Volunteer'),
+    (7, 'Internship')
+)
+
+class Skills(models.Model):
+    name= models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 
 # Create your models here.
+class JobPost(models.Model):
+    user= models.ForeignKey(User, on_delete= models.CASCADE)
+    organization= models.ForeignKey(Organization, on_delete= models.CASCADE)
+
+    title= models.CharField(max_length=100)
+    job_desc= models.TextField()
+    workplace_type= models.IntegerField(choices=WORKPLACE_TYPES)
+    location= models.CharField(max_length=100)
+    job_type= models.IntegerField(choices=WORK_TYPES)
+    skills= models.ManyToManyField(Skills)
+
+
+    created_at= models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
+    
--- organization/models.py
+++ organization/models.py
@@ -1,5 +1,9 @@
-from django.db import models
-
+from django.db import models, transaction
+from openai import OpenAI
+from dotenv import load_dotenv
+from django.contrib.auth.models import User
+load_dotenv()
+client= OpenAI()
 INDUSTRIES= (
     (1, "IT SERVICES"),
     (2, "Product Based"),
@@ -8,16 +12,50 @@ INDUSTRIES= (
 )
 # Create your models here.
 class Organization(models.Model):
+    root_user= models.OneToOneField(User, on_delete=models.CASCADE,related_name="organization_root_user")
+    users= models.ManyToManyField(User)
     name= models.CharField(max_length=100)
     industry= models.IntegerField(max_length=100, choices=INDUSTRIES)
     url= models.URLField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    knowledge= models.JSONField(blank=True, null=True)
-
-
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
+    knowledge= models.TextField(blank=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs) -> None:
+        is_being_created = self._state.adding
+        self.clean()
+        super().save(*args, **kwargs)
+
+        if is_being_created:
+            
+            assistant= client.chat.completions.create(
+                model="gpt-4o",
+                temperature=0.8,
+                messages = [
+                        {"role": "system", "content": "You are a helpful assistant skilled in analyzing organizations based on their name and website."},
+                        {
+                            "role": "user",
+                            "content": (
+                                "Analyze the organization based on its name and website to provide insights. "
+                                "Focus on:\n"
+                                "1. Company Overview: Industry, size, work environment, and values.\n"
+                                "2. Candidate Alignment: Educational qualifications, work experience, skills, cultural fit, and performance expectations.\n"
+                                "3. Additional Metrics: Diversity, adaptability, language proficiency, portfolio relevance, salary expectations, and availability.\n\n"
+                                f"Input:\n- Name: {self.name}\n- Website: {self.url} Industry: {self.industry}"
+                            )
+                        }
+]
+
+            )
+            formated_answer= assistant.choices[0].message
+            self.knowledge= formated_answer
+            self.save(update_fields=['knowledge'])
+
+            def add_member():
+                # Add the root_user of the workspace as a member
+                self.users.add(self.root_user)
+
+            # https://stackoverflow.com/a/78053539/13953998
+            transaction.on_commit(add_member)
