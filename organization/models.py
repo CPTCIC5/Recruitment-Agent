f805e210333085ce6100f5d9497bdaaa15236d3f
CPTCIC5 <aryanjainak@gmail.com>
2025-01-20 17:55:30 +0530

    xyz

--- organization/models.py
+++ organization/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+INDUSTRIES= (
+    (1, "IT SERVICES"),
+    (2, "Product Based"),
+    (3, "Finance"),
+    (4, "Sport")
+)
 # Create your models here.
+class Organization(models.Model):
+    name= models.CharField(max_length=100)
+    industry= models.IntegerField(max_length=100, choices=INDUSTRIES)
+    url= models.URLField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    knowledge= models.JSONField(blank=True, null=True)
+
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.name
