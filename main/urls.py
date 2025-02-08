f9530d4c5c175311d0309922d0fc1f3116e3f045
CPTCIC5 <aryanjainak@gmail.com>
2025-02-08 19:30:41 +0530

    xyz

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-#from .job_posting.twitter.twitter import index,redirect_to_twitter_auth
-#from job_posting.twitter.remove_token import revoke_token
+from .job_posting.twitter.twitter import index,redirect_to_twitter_auth
 
 
 urlpatterns = [
-    #path('', index, name='index'),
-    #path('', index, name='index'),
-    #path('auth/', redirect_to_twitter_auth, name='auth')
+    path('', index, name='index'),
+    path('auth/', redirect_to_twitter_auth, name='auth')
 ]
